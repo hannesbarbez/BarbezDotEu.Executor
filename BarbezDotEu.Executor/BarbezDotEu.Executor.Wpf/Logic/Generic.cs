8c5095929cf95b0c4e93fa800f0a6caa2578bc44
Hannes Barbez <hannesbarbez@hotmail.com>
2022-12-06 17:06:14 +0100

    Replace .NET FW BarbezDotEu.Executor with .NET Core BarbezDotEu.Executor.Wpf

---
+++ BarbezDotEu.Executor/BarbezDotEu.Executor.Wpf/Logic/Generic.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace BarbezDotEu.Executor.Wpf.Logic
+{
+    public static class Generic
+    {
+        public static void RunExternalProcess(string appPath, string args, bool useShellExecute, bool waitForExit)
+        {
+            ProcessStartInfo info = new(appPath, @args)
+            {
+                UseShellExecute = useShellExecute
+            };
+
+            Process p = new()
+            {
+                StartInfo = info
+            };
+            p.Start();
+            if (waitForExit) p.WaitForExit();
+        }
+
+        public static void RunExternalProcess(string appPath, string args)
+        {
+            ProcessStartInfo info = new(appPath, @args)
+            {
+                UseShellExecute = false
+            };
+
+            Process p = new()
+            {
+                StartInfo = info
+            };
+            p.Start();
+        }
+
+        public static void OpenWebsite(string url, bool anonymous)
+        {
+            Process myProcess = new();
+            try
+            {
+                // true is the default, but it is important not to set it to false
+                myProcess.StartInfo.UseShellExecute = true;
+                myProcess.StartInfo.FileName = GetDefaultBrowserPath();
+
+                //if (anonymous) myProcess.StartInfo.Arguments += "-private-window";
+                if (anonymous) myProcess.StartInfo.Arguments += "-private";
+                myProcess.StartInfo.Arguments += " " + url;
+
+                myProcess.Start();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Error");
+            }
+        }
+
+        private static string GetDefaultBrowserPath()
+        {
+            string key = @"http\shell\open\command";
+            RegistryKey registryKey = Registry.ClassesRoot.OpenSubKey(key, false);
+            return ((string)registryKey.GetValue(null, null)).Split('"')[1];
+        }
+    }
+}
---
+++ BarbezDotEu.Executor/BarbezDotEu.Executor.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using BarbezDotEu.Executor.Wpf.Logic;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BarbezDotEu.Executor.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string[] keywords;
+        private bool firstClick = true;
+        private char[] charsToSplitFrom = new char[] { ',' };
+        private int index = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenNextLinkInBrowser_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.tbInput.IsEnabled = false;
+                if (tbInput.Text != null && !string.IsNullOrEmpty(tbInput.Text))
+                {
+                    if (firstClick)
+                    {
+                        this.keywords = tbInput.Text.Split(charsToSplitFrom);
+                        firstClick = false;
+                    }
+                    if (this.keywords != null)
+                    {
+                        if (this.keywords.Length > 0)
+                        {
+                            Generic.OpenWebsite(this.keywords[this.index], true);
+                            this.index++;
+                        }
+                    }
+                }
+                else this.tbInput.IsEnabled = true;
+            }
+            catch
+            {
+                MessageBox.Show("End of the list! You opened everything. Thanks!", "All Done.", MessageBoxButton.OK);
+                this.tbInput.IsEnabled = true;
+            }
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            this.firstClick = true;
+            this.index = 0;
+            this.tbInput.IsEnabled = true;
+            this.tbInput.Clear();
+        }
+
+        private void lblAbout_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Generic.RunExternalProcess("explorer.exe", "http://" + lblAbout.Content + "/BarbezDotEu.Executor/");
+        }
+
+        private void lblAbout_MouseDown(object sender, TouchEventArgs e)
+        {
+            Generic.RunExternalProcess("explorer.exe", "http://" + lblAbout.Content + "/BarbezDotEu.Executor/");
+        }
+    }
+}
